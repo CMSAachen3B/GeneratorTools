f299d8f4e053bbed302505eb953edd4a0e1a3a6f
Tom Croote <tom.croote@gmail.com>
2017-05-10 12:08:42 +0200

    debugging, renamed gridpack

--- scripts/crabConfig_tutorial_MC_generation3.py
+++ scripts/crabConfig_tutorial_MC_generation3.py
@@ -1,15 +1,15 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'tutorial_May2015_MC_generation_2'
-config.General.workArea = '/net/scratch_cms3b/crab_projects'
+config.General.requestName = 'tutorial_May2015_MC_generation_3'
+config.General.workArea = './crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.User.voGroup = 'dcms'
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.inputFiles = ['/.automount/home/home__home2/institut_3b/croote/Documents/analysis/CMSSW_7_1_20_patch2/src/CMSAachen3B/GeneratorTools/data/ppTOzTOlep+lep-lfv_tarball.tar.xz']
+config.JobType.inputFiles = ['run_generic_tarball_cvmfs.sh','ppTOzTOleplfv_tarball.tar.xz']
 config.JobType.psetName = 'LHE.py'
 #config.JobType.allowUndistributedCMSSW = True
 config.JobType.scriptExe = 'kappaWorkflow_privateMiniAOD.sh'
@@ -22,6 +22,6 @@ NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable t
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_tutorial_May2015_MC_analysis_2'
+config.Data.outputDatasetTag = 'CRAB3_tutorial_May2015_MC_analysis_3'
 
 config.Site.storageSite = "T2_DE_DESY"
