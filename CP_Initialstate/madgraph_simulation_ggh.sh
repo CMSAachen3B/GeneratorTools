0bf51f6f40a998c07791e3ab07e69075ada7873c
jordy <jordy.degens@rwth-aachen.de>
2017-06-13 17:37:32 +0200

    madgraph simulates 3jet events

--- CP_Initialstate/madgraph_simulation_ggh.sh
+++ CP_Initialstate/madgraph_simulation_ggh.sh
@@ -2,7 +2,9 @@
 
 $MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/ggh.txt
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95" $MADGRAPH_DIR/JJ_X0/SubProcesses/makefile
+chmod u+x $MADGRAPH_DIR/JJ_X0/SubProcesses/makefile
+
+sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/JJ_X0/SubProcesses/makefile
 
 echo "hallo"
 for makefile in $MADGRAPH_DIR/JJ_X0/SubProcesses/P*/makefile;
@@ -45,6 +47,20 @@ do
 	#|| (echo "Compilation failed"; exit)
 done
 
+
+sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/JJ_X0JJJ/SubProcesses/makefile
+
+for makefile in $MADGRAPH_DIR/JJ_X0JJJ/SubProcesses/P*/makefile;
+do
+	echo `dirname $makefile`
+	echo "before"
+	cd `dirname $makefile` 
+	echo "hi"	
+	pwd
+	echo "here starts matrix2py"
+	make matrix2py.so 
+	#|| (echo "Compilation failed"; exit)
+done
 
 
 
