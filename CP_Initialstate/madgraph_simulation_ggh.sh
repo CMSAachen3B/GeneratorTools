a0e0e4a9184c570323bc40d117ccd25dddf833d9
lucas <lucas.wiens@rwth-aachen.de>
2017-07-26 16:28:53 +0200

    These script will only work if you didn't execute 'setkitanalysis' before and now it checks if you did

--- CP_Initialstate/madgraph_simulation_ggh.sh
+++ CP_Initialstate/madgraph_simulation_ggh.sh
@@ -1,67 +1,70 @@
 #!/bin/bash
 
-$MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/ggh.txt
+if [ z ${CMSSW_BASE+x} ]; then
 
-chmod u+x $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
+	$MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/ggh.txt
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
+	chmod u+x $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
 
-echo "hallo"
-for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/P*/makefile;
-do
-	echo `dirname $makefile`
-	echo "before"
-	cd `dirname $makefile`
-	echo "hi"
-	pwd
-	echo "here starts matrix2py"
-	make matrix2py.so
-	#|| (echo "Compilation failed"; exit)
-done
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/makefile
+	echo "hallo"
+	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/P*/makefile;
+	do
+		echo `dirname $makefile`
+		echo "before"
+		cd `dirname $makefile`
+		echo "hi"
+		pwd
+		echo "here starts matrix2py"
+		make matrix2py.so
+		#|| (echo "Compilation failed"; exit)
+	done
 
-for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/P*/makefile;
-do
-	echo `dirname $makefile`
-	echo "before"
-	cd `dirname $makefile`
-	echo "hi"
-	pwd
-	echo "here starts matrix2py"
-	make matrix2py.so
-	#|| (echo "Compilation failed"; exit)
-done
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/makefile
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/makefile
+	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/P*/makefile;
+	do
+		echo `dirname $makefile`
+		echo "before"
+		cd `dirname $makefile`
+		echo "hi"
+		pwd
+		echo "here starts matrix2py"
+		make matrix2py.so
+		#|| (echo "Compilation failed"; exit)
+	done
 
-for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/P*/makefile;
-do
-	echo `dirname $makefile`
-	echo "before"
-	cd `dirname $makefile`
-	echo "hi"
-	pwd
-	echo "here starts matrix2py"
-	make matrix2py.so
-	#|| (echo "Compilation failed"; exit)
-done
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/makefile
+
+	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/P*/makefile;
+	do
+		echo `dirname $makefile`
+		echo "before"
+		cd `dirname $makefile`
+		echo "hi"
+		pwd
+		echo "here starts matrix2py"
+		make matrix2py.so
+		#|| (echo "Compilation failed"; exit)
+	done
 
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/makefile
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/makefile
 
-for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/P*/makefile;
-do
-	echo `dirname $makefile`
-	echo "before"
-	cd `dirname $makefile`
-	echo "hi"
-	pwd
-	echo "here starts matrix2py"
-	make matrix2py.so
-	#|| (echo "Compilation failed"; exit)
-done
-
+	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/P*/makefile;
+	do
+		echo `dirname $makefile`
+		echo "before"
+		cd `dirname $makefile`
+		echo "hi"
+		pwd
+		echo "here starts matrix2py"
+		make matrix2py.so
+		#|| (echo "Compilation failed"; exit)
+	done
+else echo "To execute this script you must open a new terminal and not run 'setkitanalysis'!"
+fi
 
 
 #cd $MADGRAPH_DIR/gg_X0/SubProcesses/P1_gg_x0/
--- CP_Initialstate/madgraph_simulation_vbf.sh
+++ CP_Initialstate/madgraph_simulation_vbf.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-$MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/vbf.txt
+if [ -z ${CMSSW_BASE+x} ]; then
 
-chmod u+x $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/makefile
+	$MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/vbf.txt
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/makefile
+	chmod u+x $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/makefile
 
-echo "hallo"
-for makefile in $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/P*/makefile;
-do
-        echo `dirname $makefile`
-        echo "before"
-        cd `dirname $makefile`
-        echo "hi"       
-        pwd
-        echo "here starts matrix2py"
-        make matrix2py.so
-        #|| (echo "Compilation failed"; exit)
-done
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/makefile
 
-chmod u+x $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/makefile
+	echo "hallo"
+	for makefile in $MADGRAPH_DIR/data_vbf/JJ_X0JJ/SubProcesses/P*/makefile;
+	do
+	        echo `dirname $makefile`
+	        echo "before"
+	        cd `dirname $makefile`
+	        echo "hi"
+	        pwd
+	        echo "here starts matrix2py"
+	        make matrix2py.so
+	        #|| (echo "Compilation failed"; exit)
+	done
 
-sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/makefile
+	chmod u+x $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/makefile
 
-echo "hallo"
-for makefile in $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/P*/makefile;
-do
-        echo `dirname $makefile`
-        echo "before"
-        cd `dirname $makefile`
-        echo "hi"       
-        pwd
-        echo "here starts matrix2py"
-        make matrix2py.so
-        #|| (echo "Compilation failed"; exit)
+	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/makefile
 
-done
+	echo "hallo"
+	for makefile in $MADGRAPH_DIR/data_vbf/JJ_X0JJJ/SubProcesses/P*/makefile;
+	do
+	        echo `dirname $makefile`
+	        echo "before"
+	        cd `dirname $makefile`
+	        echo "hi"
+	        pwd
+	        echo "here starts matrix2py"
+	        make matrix2py.so
+	        #|| (echo "Compilation failed"; exit)
+
+	done
+
+else echo "To execute this script you must open a new terminal and not run 'setkitanalysis'!"
+fi
