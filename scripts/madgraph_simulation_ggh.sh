400f140706b393fc3cf18b9b608ad003e59f5b7a
Thomas Mueller <t.muller@cern.ch>
2017-09-01 12:37:13 +0200

    madgraph_simulation_ggh.sh: new directory structure

--- scripts/madgraph_simulation_ggh.sh
+++ scripts/madgraph_simulation_ggh.sh
@@ -1,72 +1,19 @@
 #!/bin/bash
 
-if [ z ${CMSSW_BASE+x} ]; then
-
-	$MADGRAPH_DIR/bin/mg5_aMC $GENERATOR_TOOLS/configs/ggh.txt
-
-	chmod u+x $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
-
-	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/makefile
-
-	echo "hallo"
-	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0/SubProcesses/P*/makefile;
-	do
-		echo `dirname $makefile`
-		echo "before"
-		cd `dirname $makefile`
-		echo "hi"
-		pwd
-		echo "here starts matrix2py"
-		make matrix2py.so
-		#|| (echo "Compilation failed"; exit)
-	done
-
-	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/makefile
-
-	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0J/SubProcesses/P*/makefile;
-	do
-		echo `dirname $makefile`
-		echo "before"
-		cd `dirname $makefile`
-		echo "hi"
-		pwd
-		echo "here starts matrix2py"
-		make matrix2py.so
-		#|| (echo "Compilation failed"; exit)
-	done
-
-	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/makefile
-
-	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJ/SubProcesses/P*/makefile;
-	do
-		echo `dirname $makefile`
-		echo "before"
-		cd `dirname $makefile`
-		echo "hi"
-		pwd
-		echo "here starts matrix2py"
-		make matrix2py.so
-		#|| (echo "Compilation failed"; exit)
-	done
-
-
-	sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/makefile
-
-	for makefile in $MADGRAPH_DIR/data_ggh/JJ_X0JJJ/SubProcesses/P*/makefile;
-	do
-		echo `dirname $makefile`
-		echo "before"
-		cd `dirname $makefile`
-		echo "hi"
-		pwd
-		echo "here starts matrix2py"
-		make matrix2py.so
-		#|| (echo "Compilation failed"; exit)
-	done
-else echo "To execute this script you must open a new terminal and not run 'setkitanalysis'!"
+if [ ! -d $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/ggh ]
+then
+	mkdir -p $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/ggh
 fi
 
+$CMSSW_BASE/src/CMSAachen3B/GeneratorTools/MG5_aMC_v2_5_5/bin/mg5_aMC $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/configs/ggh.txt
 
-#cd $MADGRAPH_DIR/gg_X0/SubProcesses/P1_gg_x0/
-#pwd
-#make matrix2py.so
+sed -i -e "s@^\(LINKLIBS.*=.*\)\$@\1 -L\$(CMSSW_RELEASE_BASE)/external/\$(SCRAM_ARCH)/lib/@g" $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/ggh/*/SubProcesses/makefile
+sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/ggh/*/SubProcesses/makefile
+
+for MAKEFILE in $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/ggh/*/SubProcesses/P*/makefile;
+do
+	echo `dirname $MAKEFILE`
+	cd `dirname $MAKEFILE`
+	make matrix2py.so ||  echo "COMPILATION FAILED FOR MAKEFILE \"${MAKEFILE}\"" 
+done
+
