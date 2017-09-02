0fad79e2e7674f4aebd1e45b01c75bf3349e02bb
Thomas Mueller <t.muller@cern.ch>
2017-09-02 10:18:03 +0200

    madgraph_simulation_*.sh: better formatting of output

--- scripts/madgraph_simulation_vbf_nlo.sh
+++ scripts/madgraph_simulation_vbf_nlo.sh
@@ -4,6 +4,7 @@ if [ ! -d $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/vbf_nlo ]
 then
 	mkdir -p $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/vbf_nlo
 fi
+rm -rf $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/vbf_nlo/*
 
 $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/MG5_aMC_v2_5_5/bin/mg5_aMC $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/configs/vbf_nlo.txt
 
@@ -12,8 +13,9 @@ sed -i -e "s@\(F2PY.*\)\$@\1 --fcompiler=gnu95@g" $CMSSW_BASE/src/CMSAachen3B/Ge
 
 for MAKEFILE in $CMSSW_BASE/src/CMSAachen3B/GeneratorTools/data/vbf_nlo/*/SubProcesses/P*/makefile;
 do
-	echo `dirname $MAKEFILE`
+	echo -e "\e[92mStart compiling makefile \"$MAKEFILE\"\e[0m"
 	cd `dirname $MAKEFILE`
-	make matrix2py.so ||  echo "COMPILATION FAILED FOR MAKEFILE \"${MAKEFILE}\"" 
+	make matrix2py.so && echo -e "\e[42mSuccessfully compiled makefile \"$MAKEFILE\".\e[0m" || echo -e "\e[41mFailed to compile makefile \"$MAKEFILE\"!\e[0m"
+	echo ""
 done
 
